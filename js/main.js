4df7b032d585796dbc477f279c19688088a05bc6
Andrew-DiBella <adibella@ufl.edu>
2016-03-15 22:31:51 -0700

    fix spelling errors on javascript

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var formSubmit = document.getElementByID( 'form' );
 formSubmit.addEventListent( 'submission', function() {
 	event.preventDefault();
 	console.log( 'The firstname is ' + document.getElementByID( 'firstname' ).value );
-	console.log( 'The firstname is ' + document.getElementByID( 'lastname' ).value );
-	console.log( 'The firstname is ' + document.getElementByID( 'email' ).value );
-	console.log( 'The firstname is ' + document.getElementByID( 'message' ).value );
+	console.log( 'The lastname is ' + document.getElementByID( 'lastname' ).value );
+	console.log( 'The email is ' + document.getElementByID( 'email' ).value );
+	console.log( 'The message is ' + document.getElementByID( 'message' ).value );
 } );
