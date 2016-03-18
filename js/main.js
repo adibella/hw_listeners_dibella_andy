bb8eb986406856f0034eb11b9676158b229d299d
Andrew-DiBella <adibella@ufl.edu>
2016-03-17 18:26:49 -0700

    add listener javascript and additional html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Add new article and text when the h1 element is clicked
+
+var headline = document.getElementsByTagName( 'h1' );
+
+headline.addEventListener( 'click', function() {
+	var article = document.createElement( 'article' );
+	article.innerHTML = '<p>You clicked the header!</p>';
+	document.getElementById( 'container' ).appendChild( article );
+
+} );
+
+// Displays date when key is pressed over id uno.
+
+var uno = document.getElementById( 'uno' );
+
+uno.addEventListener( 'keypress', function() {
+	document.innerHTML = '<input type="date">';
+	document.getElementsByTagName( 'h2' ).appendChild( h1 );
+
+} );
+
+//Change color of Event Listener Dos text
+
+var dos = document.getElementById( 'dos' );
+
+dos.addEventListener( 'mouseover', function() {
+	var change = document.getElementById( 'dos' );
+	change.className = 'colorchange';
+
+} )
