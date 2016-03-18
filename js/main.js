0c31d50eae21fb428f1838277bf4d339d1c954c0
Andrew-DiBella <adibella@ufl.edu>
2016-03-17 19:18:38 -0700

    clean up javascript and html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var formSubmit = document.getElementByID( 'form' );
+var formSubmit = document.getElementById( 'form' );
 
-formSubmit.addEventListent( 'submission', function() {
+formSubmit.addEventListener( 'submit', function( event ) {
 	event.preventDefault();
-	console.log( 'The firstname is ' + document.getElementByID( 'firstname' ).value );
-	console.log( 'The lastname is ' + document.getElementByID( 'lastname' ).value );
-	console.log( 'The email is ' + document.getElementByID( 'email' ).value );
-	console.log( 'The message is ' + document.getElementByID( 'message' ).value );
+	console.log( 'The firstname is ' + document.getElementById( 'firstname' ).value );
+	console.log( 'The lastname is ' + document.getElementById( 'lastname' ).value );
+	console.log( 'The email is ' + document.getElementById( 'email' ).value );
+	console.log( 'The message is ' + document.getElementById( 'message' ).value );
 } );
