68e1871a9a9a64676eb6f6823c737d70c63bcac5
Andrew-DiBella <adibella@ufl.edu>
2016-03-17 19:08:44 -0700

    readd javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var listen = document.getElementsByTagName( 'h1' );
+var num = 0;
+
+listen.addEventListener( 'click', function() {
+	num++;
+	var New = document.createElement( 'P' );
+	New.innerHTML = 'This is click number ' + New;
+	document.body.appendChild( New );
+} );
